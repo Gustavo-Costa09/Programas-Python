9588ee51bd0f6dc1814983a78a19f310055eaa54
WilliamLucas835 <Williamlucasd04@gmail.com>
2025-11-07 21:46:29 -0300

    Add files via upload

--- Lista Códigos (06-11)/Programa5/Programa5.py
+++ Lista Códigos (06-11)/Programa5/Programa5.py
@@ -1,1 +1,17 @@
+r = "s"
 
+while r == 's':
+
+	n1 = int(input ("Digite o número 1"))
+	i = 0
+	
+	while i < n1:
+		n2 = 0
+		while n2 < i+1:
+			print(n2+1, end = " ")
+			n2 += 1
+		print()
+		i += 1
+		
+	r = input("Deseja continuar (s/n)?: ")
+	
