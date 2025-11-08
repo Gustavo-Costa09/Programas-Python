1af2ee9df1073b85d126f857ff7cbb5a14f98c0c
WilliamLucas835 <Williamlucasd04@gmail.com>
2025-11-07 21:45:24 -0300

    Add files via upload

--- Lista Códigos (06-11)/Programa3/Programa3.py
+++ Lista Códigos (06-11)/Programa3/Programa3.py
@@ -1,1 +1,24 @@
+r = "s"
 
+while r == 's':
+	
+	n1 = int(input("Digite o primeiro número: "))
+	n2 = int(input("Digite o segundo número: "))
+	
+	print("Escolha um operador aritimétrico: '+', '-','*', '/'.")
+	
+	Operador = input("Digite o operador: ")
+	
+	if operador == "+":
+		print(f"O resultado é: {n1+n2}")
+	elif operador == "-":
+		print(f"O resultado é: {n1-n2")
+	elif operador == "*":
+		print(f"O resultado é: {n1*n2}")
+	elif operador == "/":
+		print (f"O resultado é: {n1/n2}")
+	else:
+		print("É necessario um operador válido"
+		
+	r = input("Deseja continuar (s/n)?:  ")
+	
